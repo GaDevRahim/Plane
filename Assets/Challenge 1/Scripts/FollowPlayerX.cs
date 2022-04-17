61224578d829839bafd0c38b9c03d1d4c0047ce3
GaDevRahim <Mahmmoud.Rahim@gmail.com>
2022-04-18 00:23:54 +0200

    Project v1.0

---
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    private GameObject plane;
+    private Vector3 offset = new Vector3(30, 0, 10);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        plane = GameObject.Find("Plane");
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
---
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private GameObject plane;
+    private GameObject fan;
+
+    private float speed = 20.0f;
+    private float rotationSpeed = 90.0f;
+    private float fanSpeed = 2000.0f;
+    private float verticalInput;
+    private float horizontalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        plane = GameObject.Find("Plane");
+        fan = GameObject.Find("Fan");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Translate(1, 0, 0);
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+
+        // get the user's horizontal input
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // rotate fan
+        fan.transform.Rotate(Vector3.forward, Time.deltaTime * fanSpeed);
+
+        // move the plane forward at a constant rate
+        plane.transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+
+        // tilt the plane up/down based on up/down arrow keys
+        plane.transform.Rotate(Vector3.right, Time.deltaTime * rotationSpeed * horizontalInput);
+        
+    }
+}
